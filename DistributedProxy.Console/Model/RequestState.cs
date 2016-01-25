5931e9d38c2a6df18cb75652995a3b974d6c193e
James Laurence <jlaurence@cafonline.org>
2016-01-25 18:49:45 +0000

    First commit

---
+++ DistributedProxy.Console/Model/RequestState.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+
+namespace DistributedProxy.Console.Model
+{
+    public class RequestState
+    {
+        // This class stores the State of the request.
+        const int BufferSize = 1024;
+        public StringBuilder RequestData { get; }
+        public byte[] BufferRead { get; }
+        public HttpWebRequest Request { get; }
+        public HttpListenerContext Context { get; }
+        public Stream StreamResponse { get; }
+        public RequestState(HttpWebRequest request, HttpListenerContext context)
+        {
+            BufferRead = new byte[BufferSize];
+            RequestData = new StringBuilder("");
+            Request = null;
+            StreamResponse = null;
+            Request = request;
+            Context = context;
+        }
+    }
+}
---
+++ DistributedProxy.Console/ProxyHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using DistributedProxy.Console.Model;
+
+namespace DistributedProxy.Console
+{
+    class ProxyHandler
+    {
+        private HttpListenerContext Context { get; }
+
+        public ProxyHandler(HttpListenerContext inputContext)
+        {
+            Context = inputContext;
+        }
+
+        public void HandleRequest()
+        {
+            
+            //Check caching local
+            //Check caching list
+            //Submit request
+            var myHttpWebRequest = (HttpWebRequest)WebRequest.Create(Context.Request.RawUrl);
+            myHttpWebRequest.KeepAlive = false;
+            myHttpWebRequest.Proxy.Credentials = CredentialCache.DefaultCredentials;
+            myHttpWebRequest.UserAgent = Context.Request.UserAgent;
+            var myRequestState = new RequestState(myHttpWebRequest, Context);
+            myHttpWebRequest.BeginGetResponse(new AsyncCallback(Response), myRequestState);
+            Thread.Sleep(10000);
+        }
+
+        private static void Response(IAsyncResult asynchronousResult)
+        {
+            System.Console.WriteLine("a");
+            var requestData = (RequestState)asynchronousResult.AsyncState;
+            using (var responseFromWebSiteBeingRelayed = (HttpWebResponse)requestData.Request.EndGetResponse(asynchronousResult))
+            {
+                using (var stream = responseFromWebSiteBeingRelayed.GetResponseStream())
+                {        
+                    stream.Close();
+                }
+            }
+        }
+    }
+}
