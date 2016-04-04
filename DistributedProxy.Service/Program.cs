8b383d43472c53e94e8583be7702b0f7dbdd3923
James Laurence <jlaurence@cafonline.org>
2016-04-04 12:08:04 +0100

    Added Windows Service

---
+++ DistributedProxy.Service/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceProcess;
+using System.Threading.Tasks;
+using DistributedProxy.Application;
+
+namespace DistributedProxy.Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            new Task(StartupHandler.Start).Start();
+            var servicesToRun = new ServiceBase[]
+            {
+                new ProxyService()
+            };
+            ServiceBase.Run(servicesToRun);
+        }
+    }
+}
---
+++ DistributedProxy.Service/ProxyService.cs
@@ -0,0 +1,33 @@
+﻿using System.ServiceProcess;
+using DistributedProxy.Application;
+
+namespace DistributedProxy.Service
+{
+    public partial class ProxyService : ServiceBase
+    {
+        public ProxyService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var proxy = new ProxyHandler();
+            var threadHandler = new ThreadHandler();
+            var installHandler = new InstallHandler();
+            proxy.Start();
+            threadHandler.StartRequestThread();
+            installHandler.Install();
+        }
+
+        protected override void OnStop()
+        {
+            var proxy = new ProxyHandler();
+            var threadHandler = new ThreadHandler();
+            var installHandler = new InstallHandler();
+            threadHandler.StopRequestThread();
+            proxy.Stop();
+            installHandler.Uninstall(true);
+        }
+    }
+}
